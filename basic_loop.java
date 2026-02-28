8d7f5f4a58400233135d3802ba17b7e1b5b2d519
Krishna chauhan <chauhan2024k@gmail.com>
2026-02-28 16:02:52 +0530

    Add files via upload

---
+++ basic_loop.java
@@ -0,0 +1,20 @@
+package DSA_LOOPS;
+import java.util.Scanner;
+public class basic_loop {
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+//        for (int i=0; i<=2; i++){
+//            System.out.println("my name is krishna chauhan");
+//        }
+ //       System.out.print("Enter the value of n:");
+//        int n = sc.nextInt();
+//        for (int i=0; i<=n; i++){
+//            System.out.println("Lets learn coding");
+//        }
+         for (int i=1; i<=100; i++){
+             if (i%2==0)
+             System.out.print(i+" ");
+         }
+
+    }
+}
